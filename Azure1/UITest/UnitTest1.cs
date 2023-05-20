8283b51e10b17a915390c5461ad9d43fd8a661ae
dallas1287 <jdthornton1@gmail.com>
2023-05-19 21:07:58 -0400

    forget that test

--- Azure1/UITest/UnitTest1.cs
+++ Azure1/UITest/UnitTest1.cs
@@ -11,25 +11,7 @@ public class UnitTest1 : IClassFixture<DriverFixture>
         _fixture = fixture;
         _driver = _fixture.Driver;
     }
-    [Fact]
-    public void Test1()
-    {
-        _driver.Navigate().GoToUrl("https://localhost:7225");
 
-        Thread.Sleep(500);
-
-        var site2Header = DriverHelpers.WaitForElement(_driver, By.XPath("/html/body/div[1]/main/article/h1"));
-        Assert.NotNull(site2Header);
-        Assert.Equal("Blazor App Number 2", site2Header.Text);
-
-        var otherSiteLink = DriverHelpers.WaitForElement(_driver, By.XPath("/html/body/div[1]/main/article/a"));
-        Assert.NotNull(otherSiteLink);
-        otherSiteLink.Click();
-
-        var site1Header = DriverHelpers.WaitForElement(_driver, By.XPath("/html/body/div[1]/main/article/h1"));
-        Assert.NotNull(site1Header);
-        Assert.Equal("Blazor App Number 1", site1Header.Text);
-    }
 
     [Fact]
     [DatabaseCleaner]
